c155b7ee9618e9cd119eacd877cc4b984af1e75a
Amit Kumar <iamamitkumar051@gmail.com>
2024-09-19 16:55:19 +0530

    added baseandromeda

--- src/contracts/helpers.ts
+++ src/contracts/helpers.ts
@@ -1,63 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import * as arbthetixMeta from '@synthetixio/v3-contracts/42161-arbthetix/meta.json';
-import * as ARBToken from '@synthetixio/v3-contracts/42161-arbthetix/ARBToken.json';
-import * as AccountProxy from '@synthetixio/v3-contracts/42161-arbthetix/AccountProxy.json';
-import * as AllErrors from '@synthetixio/v3-contracts/42161-arbthetix/AllErrors.json';
-import * as CollateralToken_ARB from '@synthetixio/v3-contracts/42161-arbthetix/CollateralToken_ARB.json';
-import * as CollateralToken_WETH from '@synthetixio/v3-contracts/42161-arbthetix/CollateralToken_WETH.json';
-import * as CoreProxy from '@synthetixio/v3-contracts/42161-arbthetix/CoreProxy.json';
-import * as DAIToken from '@synthetixio/v3-contracts/42161-arbthetix/DAIToken.json';
-import * as OracleManagerProxy from '@synthetixio/v3-contracts/42161-arbthetix/OracleManagerProxy.json';
-import * as PerpsAccountProxy from '@synthetixio/v3-contracts/42161-arbthetix/PerpsAccountProxy.json';
-import * as PerpsMarketProxy from '@synthetixio/v3-contracts/42161-arbthetix/PerpsMarketProxy.json';
-import * as PythERC7412Wrapper from '@synthetixio/v3-contracts/42161-arbthetix/PythERC7412Wrapper.json';
-import * as SpotMarketProxy from '@synthetixio/v3-contracts/42161-arbthetix/SpotMarketProxy.json';
-import * as TrustedMulticallForwarder from '@synthetixio/v3-contracts/42161-arbthetix/TrustedMulticallForwarder.json';
-import * as USDCToken from '@synthetixio/v3-contracts/42161-arbthetix/USDCToken.json';
-import * as USDProxy from '@synthetixio/v3-contracts/42161-arbthetix/USDProxy.json';
-import * as WETHToken from '@synthetixio/v3-contracts/42161-arbthetix/WETHToken.json';
-import * as SystemToken from '@synthetixio/v3-contracts/42161-arbthetix/systemToken.json';
-
-//Arb-main
-import * as ARBTokenMain from '@synthetixio/v3-contracts/42161-main/ARBToken.json';
-import * as AccountProxyMain from '@synthetixio/v3-contracts/42161-main/AccountProxy.json';
-import * as AllErrorsMain from '@synthetixio/v3-contracts/42161-main/AllErrors.json';
-import * as CollateralToken_ARBMain from '@synthetixio/v3-contracts/42161-main/CollateralToken_ARB.json';
-
-import * as CollateralToken_USDCMain from '@synthetixio/v3-contracts/42161-main/CollateralToken_USDC.json';
-import * as CollateralToken_USDeMain from '@synthetixio/v3-contracts/42161-main/CollateralToken_USDe.json';
-import * as CollateralToken_WETHMain from '@synthetixio/v3-contracts/42161-main/CollateralToken_WETH.json';
-import * as CollateralToken_sETHMain from '@synthetixio/v3-contracts/42161-main/CollateralToken_sETH.json';
-import * as CollateralToken_stBTCMain from '@synthetixio/v3-contracts/42161-main/CollateralToken_stBTC.json';
-import * as CollateralToken_tBTCMain from '@synthetixio/v3-contracts/42161-main/CollateralToken_tBTC.json';
-import * as CoreProxyMain from '@synthetixio/v3-contracts/42161-main/CoreProxy.json';
-import * as OracleManagerProxyMain from '@synthetixio/v3-contracts/42161-main/OracleManagerProxy.json';
-import * as PerpsAccountProxyMain from '@synthetixio/v3-contracts/42161-main/PerpsAccountProxy.json';
-import * as PerpsMarketProxyMain from '@synthetixio/v3-contracts/42161-main/PerpsMarketProxy.json';
-import * as PythERC7412WrapperMain from '@synthetixio/v3-contracts/42161-main/PythERC7412Wrapper.json';
-import * as RewardsDistributor_1_ARB_ARBMain from '@synthetixio/v3-contracts/42161-main/RewardsDistributor_1_ARB_ARB.json';
-import * as RewardsDistributor_1_USDC_ARBMain from '@synthetixio/v3-contracts/42161-main/RewardsDistributor_1_USDC_ARB.json';
-import * as RewardsDistributor_1_USDe_ARBMain from '@synthetixio/v3-contracts/42161-main/RewardsDistributor_1_USDe_ARB.json';
-import * as RewardsDistributor_1_WETH_ARBMain from '@synthetixio/v3-contracts/42161-main/RewardsDistributor_1_WETH_ARB.json';
-import * as RewardsDistributor_1_sETHMain from '@synthetixio/v3-contracts/42161-main/RewardsDistributor_1_sETH.json';
-import * as RewardsDistributor_1_stBTCMain from '@synthetixio/v3-contracts/42161-main/RewardsDistributor_1_stBTC.json';
-import * as SpotMarketProxyMain from '@synthetixio/v3-contracts/42161-main/SpotMarketProxy.json';
-import * as SynthToken_sETHMain from '@synthetixio/v3-contracts/42161-main/SynthToken_sETH.json';
-import * as SynthToken_sUSDCMain from '@synthetixio/v3-contracts/42161-main/SynthToken_sUSDC.json';
-import * as SynthToken_stBTCMain from '@synthetixio/v3-contracts/42161-main/SynthToken_stBTC.json';
-import * as SynthUSDCTokenMain from '@synthetixio/v3-contracts/42161-main/SynthUSDCToken.json';
-import * as TrustedMulticallForwarderMain from '@synthetixio/v3-contracts/42161-main/TrustedMulticallForwarder.json';
-import * as USDCTokenMain from '@synthetixio/v3-contracts/42161-main/USDCToken.json';
-import * as USDProxyMain from '@synthetixio/v3-contracts/42161-main/USDProxy.json';
-import * as WETHTokenMain from '@synthetixio/v3-contracts/42161-main/WETHToken.json';
-import * as CannonMain from '@synthetixio/v3-contracts/42161-main/cannon.json';
-import * as CollateralTokensMain from '@synthetixio/v3-contracts/42161-main/collateralTokens.json';
-import * as ExtrasMain from '@synthetixio/v3-contracts/42161-main/extras.json';
-import * as mainMeta from '@synthetixio/v3-contracts/42161-main/meta.json';
-import * as MintableTokensMain from '@synthetixio/v3-contracts/42161-main/mintableTokens.json';
-import * as RewardsDistributorsMain from '@synthetixio/v3-contracts/42161-main/rewardsDistributors.json';
-import * as SynthTokensMain from '@synthetixio/v3-contracts/42161-main/synthTokens.json';
-import * as SystemTokenMain from '@synthetixio/v3-contracts/42161-main/systemToken.json';
+import { abiMappingArb, metaMappingArb } from './v3-contracts/42161';
 
 // Define the structure for Meta and ABI
 interface Meta {
@@ -91,74 +33,11 @@ interface Meta {
 type ABI = Record<string, any>[];
 
 const metaMapping: Record<number, Record<string, Meta>> = {
-  42161: {
-    arbthetix: (arbthetixMeta as any).default, // Access the default export for JSON data
-    main: (mainMeta as any).default, // Access the default export for JSON data
-  },
+  42161: metaMappingArb,
 };
 
 const abiMapping: Record<number, Record<string, Record<string, ABI>>> = {
-  42161: {
-    arbthetix: {
-      ARBToken: (ARBToken as any).default,
-      AccountProxy: (AccountProxy as any).default,
-      AllErrors: (AllErrors as any).default,
-      CollateralToken_ARB: (CollateralToken_ARB as any).default,
-      CollateralToken_WETH: (CollateralToken_WETH as any).default,
-      CoreProxy: (CoreProxy as any).default,
-      DAIToken: (DAIToken as any).default,
-      OracleManagerProxy: (OracleManagerProxy as any).default,
-      PerpsAccountProxy: (PerpsAccountProxy as any).default,
-      PerpsMarketProxy: (PerpsMarketProxy as any).default,
-      PythERC7412Wrapper: (PythERC7412Wrapper as any).default,
-      SpotMarketProxy: (SpotMarketProxy as any).default,
-      TrustedMulticallForwarder: (TrustedMulticallForwarder as any).default,
-      USDCToken: (USDCToken as any).default,
-      USDProxy: (USDProxy as any).default,
-      WETHToken: (WETHToken as any).default,
-      SystemToken: (SystemToken as any).default,
-      // Add other contract ABIs as needed
-    },
-    main: {
-      ARBToken: (ARBTokenMain as any).default,
-      AccountProxy: (AccountProxyMain as any).default,
-      AllErrors: (AllErrorsMain as any).default,
-      CollateralToken_ARB: (CollateralToken_ARBMain as any).default,
-      CollateralToken_USDC: (CollateralToken_USDCMain as any).default,
-      CollateralToken_USDe: (CollateralToken_USDeMain as any).default,
-      CollateralToken_WETH: (CollateralToken_WETHMain as any).default,
-      CollateralToken_sETH: (CollateralToken_sETHMain as any).default,
-      CollateralToken_stBTC: (CollateralToken_stBTCMain as any).default,
-      CollateralToken_tBTC: (CollateralToken_tBTCMain as any).default,
-      CoreProxy: (CoreProxyMain as any).default,
-      OracleManagerProxy: (OracleManagerProxyMain as any).default,
-      PerpsAccountProxy: (PerpsAccountProxyMain as any).default,
-      PerpsMarketProxy: (PerpsMarketProxyMain as any).default,
-      PythERC7412Wrapper: (PythERC7412WrapperMain as any).default,
-      RewardsDistributor_1_ARB_ARB: (RewardsDistributor_1_ARB_ARBMain as any).default,
-      RewardsDistributor_1_USDC_ARB: (RewardsDistributor_1_USDC_ARBMain as any).default,
-      RewardsDistributor_1_USDe_ARB: (RewardsDistributor_1_USDe_ARBMain as any).default,
-      RewardsDistributor_1_WETH_ARB: (RewardsDistributor_1_WETH_ARBMain as any).default,
-      RewardsDistributor_1_sETH: (RewardsDistributor_1_sETHMain as any).default,
-      RewardsDistributor_1_stBTC: (RewardsDistributor_1_stBTCMain as any).default,
-      SpotMarketProxy: (SpotMarketProxyMain as any).default,
-      SynthToken_sETH: (SynthToken_sETHMain as any).default,
-      SynthToken_sUSDC: (SynthToken_sUSDCMain as any).default,
-      SynthToken_stBTC: (SynthToken_stBTCMain as any).default,
-      SynthUSDCToken: (SynthUSDCTokenMain as any).default,
-      TrustedMulticallForwarder: (TrustedMulticallForwarderMain as any).default,
-      USDCToken: (USDCTokenMain as any).default,
-      USDProxy: (USDProxyMain as any).default,
-      WETHToken: (WETHTokenMain as any).default,
-      SystemToken: (SystemTokenMain as any).default,
-      Cannon: (CannonMain as any).default,
-      CollateralTokens: (CollateralTokensMain as any).default,
-      Extras: (ExtrasMain as any).default,
-      MintableTokens: (MintableTokensMain as any).default,
-      RewardsDistributors: (RewardsDistributorsMain as any).default,
-      SynthTokens: (SynthTokensMain as any).default,
-    },
-  },
+  42161: abiMappingArb,
 };
 
 // Function to dynamically import metadata
---
+++ src/contracts/v3-contracts/8453/index.ts
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+//8453-andromeda
+import * as AccountProxyAndromeda from '@synthetixio/v3-contracts/8453-andromeda/AccountProxy.json';
+import * as AllErrorsAndromeda from '@synthetixio/v3-contracts/8453-andromeda/AllErrors.json';
+import * as CollateralToken_USDCAndromeda from '@synthetixio/v3-contracts/8453-andromeda/CollateralToken_USDC.json';
+import * as CoreProxyAndromeda from '@synthetixio/v3-contracts/8453-andromeda/CoreProxy.json';
+import * as OracleManagerProxyAndromeda from '@synthetixio/v3-contracts/8453-andromeda/OracleManagerProxy.json';
+import * as PerpsAccountProxyAndromeda from '@synthetixio/v3-contracts/8453-andromeda/PerpsAccountProxy.json';
+import * as PerpsMarketProxyAndromeda from '@synthetixio/v3-contracts/8453-andromeda/PerpsMarketProxy.json';
+import * as PythERC7412WrapperAndromeda from '@synthetixio/v3-contracts/8453-andromeda/PythERC7412Wrapper.json';
+import * as SpotMarketProxyAndromeda from '@synthetixio/v3-contracts/8453-andromeda/SpotMarketProxy.json';
+import * as SynthToken_sUSDCAndromeda from '@synthetixio/v3-contracts/8453-andromeda/SynthToken_sUSDC.json';
+import * as SynthUSDCTokenAndromeda from '@synthetixio/v3-contracts/8453-andromeda/SynthUSDCToken.json';
+import * as TrustedMulticallForwarderAndromeda from '@synthetixio/v3-contracts/8453-andromeda/TrustedMulticallForwarder.json';
+import * as USDCTokenAndromeda from '@synthetixio/v3-contracts/8453-andromeda/USDCToken.json';
+import * as USDProxyAndromeda from '@synthetixio/v3-contracts/8453-andromeda/USDProxy.json';
+import * as CannonAndromeda from '@synthetixio/v3-contracts/8453-andromeda/cannon.json';
+import * as CollateralTokensAndromeda from '@synthetixio/v3-contracts/8453-andromeda/collateralTokens.json';
+import * as ExtrasAndromeda from '@synthetixio/v3-contracts/8453-andromeda/extras.json';
+import * as MetaAndromeda from '@synthetixio/v3-contracts/8453-andromeda/meta.json';
+import * as MintableTokensAndromeda from '@synthetixio/v3-contracts/8453-andromeda/mintableTokens.json';
+import * as RewardsDistributorsAndromeda from '@synthetixio/v3-contracts/8453-andromeda/rewardsDistributors.json';
+import * as SynthTokensAndromeda from '@synthetixio/v3-contracts/8453-andromeda/synthTokens.json';
+import * as SystemTokenAndromeda from '@synthetixio/v3-contracts/8453-andromeda/systemToken.json';
+import * as CollateralToken_sStataUSDCAndromeda from '@synthetixio/v3-contracts/8453-andromeda/CollateralToken_sStataUSDC.json';
+import * as CollateralToken_stataBasUSDCAndromeda from '@synthetixio/v3-contracts/8453-andromeda/CollateralToken_stataBasUSDC.json';
+import * as CollateralToken_sUSDCAndromeda from '@synthetixio/v3-contracts/8453-andromeda/CollateralToken_sUSDC.json';
+import * as RewardsDistributor_1_sUSDC_SNXAndromeda from '@synthetixio/v3-contracts/8453-andromeda/RewardsDistributor_1_sUSDC_SNX.json';
+import * as RewardsDistributor_1_sUSDC_USDCAndromeda from '@synthetixio/v3-contracts/8453-andromeda/RewardsDistributor_1_sUSDC_USDC.json';
+import * as RewardsDistributorForSpartanCouncilPoolSNXAndromeda from '@synthetixio/v3-contracts/8453-andromeda/RewardsDistributorForSpartanCouncilPoolSNX.json';
+import * as RewardsDistributorForSpartanCouncilPoolUSDCAndromeda from '@synthetixio/v3-contracts/8453-andromeda/RewardsDistributorForSpartanCouncilPoolUSDC.json';
+import * as SNXTokenAndromeda from '@synthetixio/v3-contracts/8453-andromeda/SNXToken.json';
+import * as SynthToken_sStataUSDCAndromeda from '@synthetixio/v3-contracts/8453-andromeda/SynthToken_sStataUSDC.json';
+
+export const metaMappingArb = {
+  andromeda: (MetaAndromeda as any).default, // Access the default export for JSON data
+};
+
+export const abiMappingBase = {
+  andromdea: {
+    AccountProxy: (AccountProxyAndromeda as any).default,
+    AllErrors: (AllErrorsAndromeda as any).default,
+    CoreProxy: (CoreProxyAndromeda as any).default,
+    OracleManagerProxy: (OracleManagerProxyAndromeda as any).default,
+    PerpsAccountProxy: (PerpsAccountProxyAndromeda as any).default,
+    PerpsMarketProxy: (PerpsMarketProxyAndromeda as any).default,
+    PythERC7412Wrapper: (PythERC7412WrapperAndromeda as any).default,
+    SpotMarketProxy: (SpotMarketProxyAndromeda as any).default,
+    SynthToken_sUSDC: (SynthToken_sUSDCAndromeda as any).default,
+    SynthUSDCToken: (SynthUSDCTokenAndromeda as any).default,
+    TrustedMulticallForwarder: (TrustedMulticallForwarderAndromeda as any).default,
+    USDCToken: (USDCTokenAndromeda as any).default,
+    USDProxy: (USDProxyAndromeda as any).default,
+    cannon: (CannonAndromeda as any).default,
+    collateralTokens: (CollateralTokensAndromeda as any).default,
+    extras: (ExtrasAndromeda as any).default,
+    mintableTokens: (MintableTokensAndromeda as any).default,
+    rewardsDistributors: (RewardsDistributorsAndromeda as any).default,
+    synthTokens: (SynthTokensAndromeda as any).default,
+    systemToken: (SystemTokenAndromeda as any).default,
+    CollateralToken_USDC: (CollateralToken_USDCAndromeda as any).default,
+    CollateralToken_sStataUSDC: (CollateralToken_sStataUSDCAndromeda as any).default,
+    CollateralToken_stataBasUSDC: (CollateralToken_stataBasUSDCAndromeda as any).default,
+    CollateralToken_sUSDC: (CollateralToken_sUSDCAndromeda as any).default,
+    RewardsDistributor_1_sUSDC_SNX: (RewardsDistributor_1_sUSDC_SNXAndromeda as any).default,
+    RewardsDistributor_1_sUSDC_USDC: (RewardsDistributor_1_sUSDC_USDCAndromeda as any).default,
+    RewardsDistributorForSpartanCouncilPoolSNX: (RewardsDistributorForSpartanCouncilPoolSNXAndromeda as any).default,
+    RewardsDistributorForSpartanCouncilPoolUSDC: (RewardsDistributorForSpartanCouncilPoolUSDCAndromeda as any).default,
+    SNXToken: (SNXTokenAndromeda as any).default,
+    SynthToken_sStataUSDC: (SynthToken_sStataUSDCAndromeda as any).default,
+  },
+};
