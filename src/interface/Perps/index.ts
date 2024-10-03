383f6d7060f25a44db64db1e61f0dc32a2296cdb
racso <oscaremiliolugo@gmail.com>
2024-10-03 00:17:49 -0400

    typing classes

--- src/interface/Perps/index.ts
+++ src/interface/Perps/index.ts
@@ -1,9 +1,9 @@
+import { MarketIdOrName } from '../commonTypes';
+
 export type CommitOrder = {
   size: number;
   settlementStrategyId: number;
-  marketId?: number;
-
-  marketName?: string;
+  marketIdOrName: MarketIdOrName;
   accountId?: bigint;
   desiredFillPrice?: number;
   maxPriceImpact?: number;
@@ -11,7 +11,31 @@ export type CommitOrder = {
 
 export type ModifyCollateral = {
   amount: number;
-  marketId?: number;
-  marketName?: string;
+  marketIdOrName: MarketIdOrName;
   accountId?: bigint;
+};
+
+export type CreateIsolateOrder = {
+  collateralAmount: number;
+  collateralMarketId: number;
+  size: number;
+  marketIdOrName: MarketIdOrName;
+  settlementStrategyId: number;
+  accountId?: bigint;
+  desiredFillPrice?: number;
+  maxPriceImpact?: number;
+};
+
+export type GetPerpsQuote = {
+  size: number;
+  price?: number;
+  marketIdOrName: MarketIdOrName;
+  accountId?: bigint;
+  settlementStrategyId?: number;
+  includeRequiredMargin?: boolean;
+};
+
+export type PayDebt = {
+  accountId?: bigint;
+  amount: number;
 };
--- src/interface/commonTypes.ts
+++ src/interface/commonTypes.ts
@@ -4,12 +4,17 @@ import { Call3Value } from './contractTypes';
 export type OverrideParamsWrite = {
   shouldRevertOnTxFailure?: boolean;
   submit?: boolean;
+  useMultiCall?: boolean;
+  staticCall?: boolean;
+  txDelay?: number;
+  maxTries?: number;
 };
 
-export type OverrideParamsRead = {
-  shouldRevertOnTxFailure?: boolean;
-  submit?: boolean;
-};
+export type OverrideParamsRead = unknown;
+// {
+// shouldRevertOnTxFailure?: boolean;
+// submit?: boolean;
+// };
 
 export type WriteCallParams = {
   calls: Call3Value[];
@@ -26,3 +31,7 @@ export type WriteContractParams = {
 export type WriteErc7412 = WriteCallParams | WriteContractParams;
 
 export type WriteReturnType = string | CallParameters;
+
+export type MarketIdOrName = number | string;
+
+export type ReturnWriteCall = string | CallParameters | CallParameters[];
