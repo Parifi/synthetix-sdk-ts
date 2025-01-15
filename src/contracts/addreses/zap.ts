f26c5fc932f60b7c3af3b49369058a5228835732
racso <oscaremiliolugo@gmail.com>
2025-01-15 10:27:56 -0400

    include zao addresses

--- src/contracts/addreses/zap.ts
+++ src/contracts/addreses/zap.ts
@@ -1,6 +1,9 @@
-import { Address } from 'viem';
+import { Address, zeroAddress } from 'viem';
+import { SUPPORTED_CHAINS } from '../../constants/chains';
 
 export const ZAP_BY_CHAIN: Record<number, Address> = {
-  [421614]: '0xb569ed692206a1d73996088ae646333b1d59d9c5',
-  [42161]: '0x9ec181B2E69fB36C50031F0c87Bc0749b766A9f4',
+  [SUPPORTED_CHAINS.BASE]: '0xf2357286905c3b36454fa9f6f969d2807ee78952',
+  [SUPPORTED_CHAINS.BASE_SEPOLIA]: zeroAddress,
+  [SUPPORTED_CHAINS.ARBITRUM]: '0x9ec181B2E69fB36C50031F0c87Bc0749b766A9f4',
+  [SUPPORTED_CHAINS.ARBITUM_SEPOLIA]: '0xb569ed692206a1d73996088ae646333b1d59d9c5',
 };
